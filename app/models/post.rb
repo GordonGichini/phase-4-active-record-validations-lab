897f768c025f5cc6ea6cf2c8f4241858ee0dc5af
GordonGichini <gordongichini1@gmail.com>
2022-12-17 02:44:29 +0300

    commit tree c25c7d3c7424e9df010d6c6b1fa68d7d7a3eb079

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
-class Post < ApplicationRecord
+class Post < ApplicationRecord 
+    validates :title, presence: true
+    validate :clickbait_title
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 } 
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+        message: "is not included in the list" }
+
+    private 
+
+    def clickbait_title 
+        unless title.present? && (title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess"))
+            errors.add(:title, "must be sufficiently clickbait-y")
+        end
+    end 
 end
